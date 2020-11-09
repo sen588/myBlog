93d52c6fce48abd87439c90c17f9a5ed28363a5c
sen588 <lisen19980328>
2020-11-09 12:56:39 +0800

    博客初始化

---
+++ bs_blog_aw/src/main/java/com/blog/aw/BsBlogAwApplication.java
@@ -0,0 +1,13 @@
+package com.blog.aw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsBlogAwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsBlogAwApplication.class, args);
+    }
+
+}
